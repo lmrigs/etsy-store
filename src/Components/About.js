dd909746a09726d407fa5b61c6568f0e01812105
lrigs <lauramariganello@gmail.com>
2023-08-17 05:09:50 -0400

    updates to about and contact

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,35 +1,26 @@
 import "../Styles/About.scss";
 
 export default function About() {
-  const images = [
-    {
-      original: require("../Images/Gallery/Rectangle-18.png"),
-      thumbnail: require("../Images/Gallery/Rectangle-18.png"),
-      originalAlt: "bracelets",
-    },
-    {
-      original: require("../Images/Gallery/Rectangle-19.png"),
-      thumbnail: require("../Images/Gallery/Rectangle-19.png"),
-      originalAlt: "bracelets",
-    },
-  ];
-
   return (
     <div className="about-container" id="about">
       <h2>About Us</h2>
-      <div className="about-text">
-        <p>
-          Our company is a family-owned business that specializes in creating
-          handmade leather goods.
-        </p>
-        <p id="right-align">
-          We are passionate about crafting unique and stunning pieces that are
-          not only beautiful but also durable.
-        </p>
-        <p>
-          Our skilled artisans will work with you to thoughtfully customize your
-          piece.
-        </p>
+      <div className="content-container">
+        <div className="creator-image"></div>
+
+        <div className="about-text">
+          <p>
+            Our company is a family-owned business that specializes in creating
+            handmade leather goods.
+          </p>
+          <p id="right-align">
+            We are passionate about crafting unique and stunning pieces that are
+            not only beautiful but also durable.
+          </p>
+          <p>
+            Our skilled artisans will work with you to thoughtfully customize
+            your piece.
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,4 +1,8 @@
 import "../Styles/Products.scss";
+import featuredImage from "../Images/aromateec--q9tuxLn4hA-unsplash.jpg";
+import featuredImage2 from "../Images/audrey-shattuck-O2c-AiowrCE-unsplash.jpg";
+import featuredImage3 from "../Images/konstantin-evdokimov-rhP8U-86GZA-unsplash.jpg";
+import featuredImage4 from "../Images/kimia-zarifi-a6MjyEtg9cQ-unsplash.jpg";
 
 function Category(props) {
   return (
@@ -20,18 +24,22 @@ export default function Products() {
   return (
     <div className="product-list" id="products">
       <Category
+        imageSrc={featuredImage}
         text="Featured Category"
         etsy="https://www.etsy.com/ca/listing/659971493/laminated-wood-cuff-bracelets?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=hardwood+bracelets&ref=sr_gallery-1-1&organic_search_click=1"
       />
       <Category
+        imageSrc={featuredImage2}
         text="Featured Category 2"
         etsy="https://www.etsy.com/ca/listing/659971493/laminated-wood-cuff-bracelets?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=hardwood+bracelets&ref=sr_gallery-1-1&organic_search_click=1"
       />
       <Category
+        imageSrc={featuredImage3}
         text="Featured Category 3"
         etsy="https://www.etsy.com/ca/listing/659971493/laminated-wood-cuff-bracelets?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=hardwood+bracelets&ref=sr_gallery-1-1&organic_search_click=1"
       />
       <Category
+        imageSrc={featuredImage4}
         text="Featured Category 4"
         etsy="https://www.etsy.com/ca/listing/659971493/laminated-wood-cuff-bracelets?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=hardwood+bracelets&ref=sr_gallery-1-1&organic_search_click=1"
       />
